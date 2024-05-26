948a28913ae3d062126ea24034ba21ad30aba4c5
nari1100 <sompallinari77@gmail.com>
2024-05-26 14:27:37 +0530

    logging part update

--- src/MCQ/logger.py
+++ src/MCQ/logger.py
@@ -1,1 +1,18 @@
 
+import logging
+import logging.config
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+
+logpath=os.path.join(os.getcwd(),"logs")
+
+os.makedirs(logpath,exist_ok=True)
+
+LOG_FILEPATH=os.path.join(logpath,LOG_FILE)
+logging.basicConfig(
+    level=logging.INFO,
+    filename=LOG_FILEPATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
---
+++ test.py
@@ -0,0 +1,3 @@
+from src.MCQ.logger import logging
+
+logging.info("I am going to sart project")
